8d5ca422e19da06f22366b020ae628c06c5a23ae
su8ru <s@su8ru.dev>
2020-04-08 13:45:51 +0900

    feat: prepare header, router-view, footer

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,45 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <header>
+      <h1>I Will Pay With ...</h1>
+    </header>
+    <router-view></router-view>
+    <footer>
+      <small>
+        &copy; su8ru, kichi2004, takeno-hito 2020
+      </small>
+    </footer>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+#app {
+  display: grid;
+  grid-template-rows: 2.5rem 1fr 30px;
+  grid-template-columns: 1fr;
+  height: var(--vh);
+  text-align: center;
+}
+</style>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 
 @Component
-export default class Layout extends Vue {}
+export default class Layout extends Vue {
+  height: number = 0;
+
+  resize() {
+    this.height = window.innerHeight;
+    document.documentElement.style.setProperty("--vh", `${this.height}px`);
+  }
+
+  mounted() {
+    this.resize();
+    window.addEventListener("resize", this.resize);
+  }
+  beforeDestroy() {
+    window.removeEventListener("resize", this.resize);
+  }
+}
 </script>
